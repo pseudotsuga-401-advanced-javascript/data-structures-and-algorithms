e0a78d1d693b3ed3a36b2273fcf8c018a4a1dfd9
Conor McCue <conormccue@gmail.com>
2020-01-22 14:06:54 -0800

    add stack class

--- stacks-and-queues/stacks-and-queues.js
+++ stacks-and-queues/stacks-and-queues.js
@@ -6,3 +6,26 @@ class Node {
     this.value = value;
   }
 }
+
+class Stack {
+  constructor(top = null){
+    this.top = top
+  }
+  peek(){
+    return this.top;
+  }
+  pop(){
+    let temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    return temp;
+  }
+  push(value){
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+  }
+  isEmpty(){
+    this.top === null ? true : false;
+  }
+}
