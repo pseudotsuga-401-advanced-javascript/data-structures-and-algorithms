c0c5c6b11d4d40a20d99502b34e82267e82e26ba
Conor McCue <conormccue@gmail.com>
2020-01-23 23:41:08 -0800

    remove comments

--- data-structures/linkedList/__tests__/linked-list.test.js
+++ data-structures/linkedList/__tests__/linked-list.test.js
@@ -1,12 +1,7 @@
 'use strict';
 
 const {Node, LinkedList} = require('../linked-list.js');
-// Can successfully add a node to the end of the linked list
-// Can successfully add multiple nodes to the end of a linked list
-// Can successfully insert a node before a node located i the middle of a linked list
-// Can successfully insert a node before the first node of a linked list
-// Can successfully insert after a node in the middle of the linked list
-// Can successfully insert a node after the last node of the linked list
+
 
 // TODO: Dry up this test suite
 describe('Tests for linked lists', () => {
